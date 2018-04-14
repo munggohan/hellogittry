79f51d70d708e10e9d819d457812e9be3bba6eff
munggohan <38007456+munggohan@users.noreply.github.com>
2018-04-14 13:33:35 +0800

    Update Practicegit.java

--- src/practicegit/Practicegit.java
+++ src/practicegit/Practicegit.java
@@ -16,6 +16,7 @@ public class Practicegit {
      */
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Nagmamahal, ate Lec");
     }
     
 }
